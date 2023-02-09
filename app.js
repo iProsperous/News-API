75f51249633fb270fbd3cdac3c59ec6bbc169dd1
iProsperous <43316663+iProsperous@users.noreply.github.com>
2023-02-09 09:33:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -22,34 +22,6 @@ function customHttp() {
         cb(error);
       }
     },
-    post(url, body, headers, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.addEventListener('load', () => {
-          if (Math.floor(xhr.status / 100) !== 2) {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
-          }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
-
-        xhr.addEventListener('error', () => {
-          cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
-
-        if (headers) {
-          Object.entries(headers).forEach(([key, value]) => {
-            xhr.setRequestHeader(key, value);
-          });
-        }
-
-        xhr.send(JSON.stringify(body));
-      } catch (error) {
-        cb(error);
-      }
-    },
   };
 }
 // Init http module
@@ -60,7 +32,7 @@ const http = customHttp();
 
 const newsService = (function() {
   const apiKey = '49270792686b4d7eacd9023faaf7e133';
-  const apiUrl = 'http://newsapi.org/v2/';
+  const apiUrl = 'https://newsapi.org/v2/';
 
   return {
     topHeadlines(country = "ru", callback) {
